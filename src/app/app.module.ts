01b9e10c38a5c4564ce7ad46dff2caf46ab7ffb1
naopeke <naopeke@protonmail.com>
2023-12-12 13:18:17 +0100

    reto 6,7,8 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormRegisterComponent } from './components/form-register/form-register.
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    FormRegisterComponent
+    FormRegisterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
