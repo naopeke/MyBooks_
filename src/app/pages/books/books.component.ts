a9a224a5192d8e5c2c4e967e7fb84175ec16648a
naopeke <naopeke@protonmail.com>
2024-01-20 12:40:34 +0100

    changed to the new method of subscribe

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -33,7 +33,7 @@ export class BooksComponent implements OnInit {
     this.apiService.books = null;
   }
 
-  //Codigo Error para que use  next, error, complete (Aunque he comprobado si funciona los ambos)
+//Codigo Error salió para que usa  next, error, complete (Aunque he comprobado si funciona los ambos)
 //'(next?: (value: Object) => void, error?: (error: any) => void, complete?: () => void): Subscription' is deprecated.ts(6385)
 //Observable.d.ts(51, 9): The declaration was marked as deprecated here.
 //(method) Observable<Object>.subscribe(next?: (value: Object) => void, error?: (error: any) => void, complete?: () => void): Subscription (+1 overload)
@@ -49,6 +49,53 @@ export class BooksComponent implements OnInit {
   //   }, error => {
   //     console.error('Error:', error);
   //   });
+  // }
+
+  // mostrarUnLibro(searchInput: string):void{
+  //   if(searchInput === ''){
+  //     this.apiService.getAll().subscribe((resp: Respuesta) =>{
+  //       this.books = resp.data;
+  //     });
+
+  //   } else {
+  //     let bookId = parseFloat(searchInput);
+  //     this.apiService.getOne(bookId).subscribe((resp:Respuesta) =>{
+  //       console.log(resp);
+  //       if(resp.error){
+  //         this.toastr.warning('No existe este codigo.', 'Error',
+  //         {timeOut:2000, positionClass:'toast-top-center'});
+  //       }
+  //       else
+  //         this.apiService.books = resp.data;
+  //         this.router.navigate(['/books', bookId]);
+  //     });
+  //   }
+  //   }
+
+    // eliminarLibro(bookId: number): void {
+  //   this.apiService.delete(bookId).subscribe((resp: Respuesta) => {
+  //     console.log(resp);
+  //     this.books = this.books.filter(book => book.id_book !== bookId);
+  //     this.toastr.success('Libro eliminado correctamente', 'Success');
+  //   }, error => {
+  //     console.error('Error:', error);
+  //     this.toastr.error('No se pudo eliminar el libro', 'Error');
+  //   });
+  // }
+
+  // detalleLibro(): void {
+  //   // Obtener bookId desde URL parametro (lo que nos enseñó José)
+  //   // this.parametro = this.rutaActiva.snapshot.params.bookid;
+  
+  //   if (this.parametro) {
+  //     let bookId = parseFloat(this.parametro);
+  //     this.apiService.getOne(bookId).subscribe((resp: Respuesta) => {
+  //       console.log("API Response:", resp);
+  //       // data:Array [{...}] asi que hay que elegir data[0]↓
+  //       this.bookDetail = resp.data[0];
+  //       console.log("Book Detail:", this.bookDetail);
+  //     });
+  //   }
   // }
 
 
@@ -70,51 +117,41 @@ export class BooksComponent implements OnInit {
 
 
   mostrarUnLibro(searchInput: string):void{
-    // cuando search bar está vacio, muestra todos los libros.
     if(searchInput === ''){
-      this.apiService.getAll().subscribe((resp: Respuesta) =>{
-        this.books = resp.data;
+      this.apiService.getAll().subscribe({
+        next: (resp: Respuesta) =>{
+          this.books = resp.data;
+        },
+        error: error => {
+          console.error('Error:', error);
+        }
       });
 
     } else {
-
       let bookId = parseFloat(searchInput);
-      this.apiService.getOne(bookId).subscribe((resp:Respuesta) =>{
-        console.log(resp);
+      this.apiService.getOne(bookId).subscribe({
+        next: (resp:Respuesta) =>{
+          console.log(resp);
         // si no puede obtener id adecuado, warning
-        if(resp.error){
-          this.toastr.warning('No existe este codigo.', 'Error',
-          {timeOut:2000, positionClass:'toast-top-center'});
-        }
+          if(resp.error){
+            this.toastr.warning('No existe este codigo.', 'Error',
+            {timeOut:2000, positionClass:'toast-top-center'});
+          } else {
         // si ha podido obtener id
-        else
-          this.apiService.books = resp.data;
-          this.router.navigate(['/books', bookId]);
-      });
+            this.apiService.books = resp.data;
+            this.router.navigate(['/books', bookId]);
+          }
+        },
+        error: error => {
+          console.error(error);
+        },
+        complete: () => {
+          console.log('Completed');
+        }
+    });
     }
-    }
+  }
 
-    
-
-//Codigo Error para que use  next, error, complete
-//'(next?: (value: Object) => void, error?: (error: any) => void, complete?: () => void): Subscription' is deprecated.ts(6385)
-//Observable.d.ts(51, 9): The declaration was marked as deprecated here.
-//(method) Observable<Object>.subscribe(next?: (value: Object) => void, error?: (error: any) => void, complete?: () => void): Subscription (+1 overload)
-//@deprecated — Instead of passing separate callback arguments, use an observer argument. Signatures taking separate callback arguments will be removed in v8. Details: https://rxjs.dev/deprecations/subscribe-arguments
-
-//https://dev.to/barisbll/rxjs-7-observable-observer-and-subscription-37fk
-
-
-  // eliminarLibro(bookId: number): void {
-  //   this.apiService.delete(bookId).subscribe((resp: Respuesta) => {
-  //     console.log(resp);
-  //     this.books = this.books.filter(book => book.id_book !== bookId);
-  //     this.toastr.success('Libro eliminado correctamente', 'Success');
-  //   }, error => {
-  //     console.error('Error:', error);
-  //     this.toastr.error('No se pudo eliminar el libro', 'Error');
-  //   });
-  // }
     
   eliminarLibro(bookId: number): void {
   this.apiService.delete(bookId).subscribe({
@@ -131,22 +168,28 @@ export class BooksComponent implements OnInit {
       console.log('Completed');
     }
   });
-}
-
+  }
 
 
   detalleLibro(): void {
-    // Obtener bookId desde URL parametro (lo que nos enseñó José)
-    // this.parametro = this.rutaActiva.snapshot.params.bookid;
-  
     if (this.parametro) {
       let bookId = parseFloat(this.parametro);
-      this.apiService.getOne(bookId).subscribe((resp: Respuesta) => {
-        console.log("API Response:", resp);
-        // data:Array [{...}] asi que hay que elegir data[0]↓
-        this.bookDetail = resp.data[0];
-        console.log("Book Detail:", this.bookDetail);
+      this.apiService.getOne(bookId).subscribe({
+        next: (resp: Respuesta) => {
+          console.log("API Response:", resp);
+          //Está metido el dato como data:Array [{...}] asi que hay que elegir data[0]↓
+          this.bookDetail = resp.data[0];
+          console.log("Book Detail:", this.bookDetail);
+        },
+        error: error => {
+          console.error('Error:', error);
+        },
+        complete: () => {
+          console.log('Completed');
+        }
       });
+    } else {
+      console.log('Error occured');
     }
   }
   
